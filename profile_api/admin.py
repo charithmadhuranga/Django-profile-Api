84e34490fe0a20f944026a3bf57354fa566c9821
charithmadhuranga <nikhuge@gmail.com>
2025-04-07 16:39:00 +0530

    add get and post request functions views

--- profile_api/admin.py
+++ profile_api/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from profile_api import models
 
 # Register your models here.
+class UserProfileAdmin(UserAdmin):
+    """User Profile Admin"""
+    list_display = ('email','first_name','last_name','is_superuser','is_staff')
+    readonly_fields = ('created_at','updated_at','last_login')
+    ordering = ('-created_at',)
+    list_filter = ('is_superuser','is_staff')
+    search_fields = ('email',)
+    filter_horizontal = ()
+    fieldsets = ()
+
+
+admin.site.register(models.UserProfile, UserProfileAdmin)
--- profile_api/views.py
+++ profile_api/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import serializers, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from profile_api import serializers
 
 # Create your views here.
+
+class HelloApiView(APIView):
+    """Hello APIView"""
+    serializer_class = serializers.HelloSerializer
+
+    def get(self, request, format=None):
+        an_apiview = [
+            'Uses HTTP methods as function (get, post, patch, put, delete)',
+            'Is similar to a traditional Django view',
+            'Gives you the most control over your application logic',
+            'Is mapped manually to urls'
+        ]
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """create a post request"""
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
